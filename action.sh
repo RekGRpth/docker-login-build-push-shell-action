b27ffdcd96c1e9594be6fb6c62904b318d6cdee8
RekGRpth <rekgrpth@gmail.com>
2021-08-05 20:41:03 +0500

    up

--- action.sh
+++ action.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -eux
-env | sort
 REGISTRY_USERNAME_IMAGE_TAG="$(echo "${INPUTS_REGISTRY:-ghcr.io}/${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}/${INPUTS_IMAGE:-${GITHUB_REPOSITORY##*/}}:${INPUTS_TAG:-${GITHUB_REF##*/}}" | tr '[:upper:]' '[:lower:]')"
 echo "${INPUTS_PASSWORD:-${GITHUB_TOKEN}}" | docker login --username "${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}" --password-stdin "${INPUTS_REGISTRY:-ghcr.io}"
 docker build --progress=plain --tag "${REGISTRY_USERNAME_IMAGE_TAG}" .
