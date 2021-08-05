927d8452b7989c6ac027c933361138e90f453d64
RekGRpth <rekgrpth@gmail.com>
2021-08-05 16:10:39 +0500

    op

--- action.sh
+++ action.sh
@@ -2,7 +2,7 @@
 
 set -eux
 env | sort
-REGISTRY_USERNAME_IMAGE_TAG="$(echo "${INPUTS_REGISTRY}/${INPUTS_USERNAME}/${INPUTS_IMAGE:-${GITHUB_REPOSITORY##*/}}:${INPUTS_TAG:-${GITHUB_REF##*/}}" | tr '[:upper:]' '[:lower:]')"
-docker login --username "${INPUTS_USERNAME}" --password "${INPUTS_PASSWORD}" "${INPUTS_REGISTRY}"
+REGISTRY_USERNAME_IMAGE_TAG="$(echo "${INPUTS_REGISTRY:-ghcr.io}/${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}/${INPUTS_IMAGE:-${GITHUB_REPOSITORY##*/}}:${INPUTS_TAG:-${GITHUB_REF##*/}}" | tr '[:upper:]' '[:lower:]')"
+docker login --username "${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}" --password "${INPUTS_PASSWORD:-${GITHUB_TOKEN}}" "${INPUTS_REGISTRY:-ghcr.io}"
 docker build --progress=plain --tag "${REGISTRY_USERNAME_IMAGE_TAG}" .
 docker push "${REGISTRY_USERNAME_IMAGE_TAG}"
