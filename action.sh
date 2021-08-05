6efd13935bf53745cc8ceb7a3282366918ea0241
RekGRpth <rekgrpth@gmail.com>
2021-08-05 15:29:29 +0500

    release

--- action.sh
+++ action.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -eux
+env | sort
 REGISTRY_USERNAME_IMAGE_TAG="$(echo "${INPUTS_REGISTRY}/${INPUTS_USERNAME}/${INPUTS_IMAGE:-${GITHUB_REPOSITORY##*/}}:${INPUTS_TAG:-${GITHUB_REF##*/}}" | tr '[:upper:]' '[:lower:]')"
 docker login --username "${INPUTS_USERNAME}" --password "${INPUTS_PASSWORD}" "${INPUTS_REGISTRY}"
 docker build --progress=plain --tag "${REGISTRY_USERNAME_IMAGE_TAG}" .
