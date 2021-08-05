031f80a430da69da59ab89bcab1ec94e72e7bcd0
RekGRpth <rekgrpth@gmail.com>
2021-08-05 16:55:33 +0500

    password-stdin

--- action.sh
+++ action.sh
@@ -3,6 +3,6 @@
 set -eux
 env | sort
 REGISTRY_USERNAME_IMAGE_TAG="$(echo "${INPUTS_REGISTRY:-ghcr.io}/${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}/${INPUTS_IMAGE:-${GITHUB_REPOSITORY##*/}}:${INPUTS_TAG:-${GITHUB_REF##*/}}" | tr '[:upper:]' '[:lower:]')"
-docker login --username "${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}" --password "${INPUTS_PASSWORD:-${GITHUB_TOKEN}}" "${INPUTS_REGISTRY:-ghcr.io}"
+echo "${INPUTS_PASSWORD:-${GITHUB_TOKEN}}" | docker login --username "${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}" --password-stdin "${INPUTS_REGISTRY:-ghcr.io}"
 docker build --progress=plain --tag "${REGISTRY_USERNAME_IMAGE_TAG}" .
 docker push "${REGISTRY_USERNAME_IMAGE_TAG}"
