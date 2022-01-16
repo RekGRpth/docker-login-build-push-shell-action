c25bea6b801d56addb9033da512233222cf53f0f
RekGRpth <rekgrpth@gmail.com>
2022-01-16 13:20:06 +0500

    up

--- action.sh
+++ action.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 set -eux
+export BUILDKIT_STEP_LOG_MAX_SIZE=-1
+export BUILDKIT_STEP_LOG_MAX_SPEED=-1
 export DOCKER_BUILDKIT=1
 REGISTRY_USERNAME_IMAGE_TAG="$(echo "${INPUTS_REGISTRY:-ghcr.io}/${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}/${INPUTS_IMAGE:-${GITHUB_REPOSITORY##*/}}:${INPUTS_TAG:-${GITHUB_REF##*/}}" | tr '[:upper:]' '[:lower:]')"
 echo "${INPUTS_PASSWORD:-${GITHUB_TOKEN}}" | docker login --username "${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}" --password-stdin "${INPUTS_REGISTRY:-ghcr.io}"
